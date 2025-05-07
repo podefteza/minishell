7c431e0f91e93026dd75c066bb41ed6cfde0d8ce
Pedro Miguel Soares Simoes <pesoares@c2r1s7.42porto.com>
2025-05-07 19:30:41 +0100

    I added a folder with a script a friend sent to me, it shows some leaks in our project

---
+++ pedro script/run_autotests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#set -x
+test_file="test_commands.txt"
+
+while IFS= read -r command; do
+    echo "Executing command: $command"
+    echo "$command" | valgrind -q --leak-check=full --show-leak-kinds=all --suppressions=readline_supression ./minishell
+    #echo "$command" | ./minishell
+done < "$test_file"
